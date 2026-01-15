3774265f13fe0e1f141ab9fb01cb1f99e6cc0802
ricard0g <ricardoguzman444@gmail.com>
2026-01-15 14:36:24 +0100

    refactor: Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,9 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-      plugins: [tailwindcss()],
+	vite: {
+		plugins: [tailwindcss()],
 	},
-
-  integrations: [sitemap()],
+	site: "https://ricardoguzman.dev",
+	integrations: [sitemap()],
 });
